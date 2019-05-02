d98a28f07fc4f193627617cc6c6a44e08dd99851
Patrick Uiterwijk <patrick@puiterwijk.org>
2019-05-02 13:58:12 +0200

    feature
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- server/protobuf/pingmessage.go
+++ server/protobuf/pingmessage.go
@@ -3,14 +3,22 @@ package protobuf
 // FeatureBit is the type for feature bits
 type FeatureBit uint64
 
-const ()
+const (
+	// FeatureBitPackSync indicates support for "packsync", which syncs pushes as a single pack file
+	FeatureBitPackSync FeatureBit = 0
+)
+
+func featureBitValue(bit FeatureBit) uint64 {
+	return uint64(1 << bit)
+}
 
 // HasFeature returns whether a particular bit is set in the ping message
 func (p *PingMessage) HasFeature(feature FeatureBit) bool {
-	return p.GetFeatureBits()&uint64(feature) != 0
+	return p.GetFeatureBits()&featureBitValue(feature) != 0
 }
 
 // SupportedFeatures returns the set of features supported by this build
 func SupportedFeatures() uint64 {
-	return uint64(0)
+	return (featureBitValue(FeatureBitPackSync) |
+		0)
 }
